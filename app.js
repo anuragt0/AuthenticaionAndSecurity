3a1102cb23ed7b6bbb6f4e2b1541d784586f6221
Anurag <anuragtiw112@gmail.com>
2022-03-26 11:18:15 +0530

    bcrypt

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption')
+const bcrypt = require('bcrypt');
+const saltRounds= 10;
 
 const app = express();
 
-console.log(process.env.API_KEY);
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
@@ -23,8 +23,6 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
-
 const User = new mongoose.model("User", userSchema);
 
 app.get('/', (req,res)=>{
@@ -38,17 +36,20 @@ app.get('/register', (req,res)=>{
 })
 
 app.post('/register', (req,res)=>{
-    const newUser = new User({
-        email: req.body.username,
-        password: req.body.password
-    });
-    newUser.save((err)=>{
-        if(err){
-            console.log(err);
-        }
-        else{
-            res.render('secrets');
-        }
+    bcrypt.hash(req.body.password, saltRounds, (err, hash)=>{
+        console.log("Here we go");
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+        newUser.save((err)=>{
+            if(err){
+                console.log(err);
+            }
+            else{
+                res.render('secrets');
+            }
+        })
     })
 })
 
@@ -61,9 +62,11 @@ app.post('/login', (req, res)=>{
             console.log(err);
         }
         else{
-            if(foundUser && foundUser.password===password){
-                console.log(foundUser.password);
-                res.render('secrets')
+            if(foundUser){
+                bcrypt.compare(password, foundUser.password, (err, result)=>{
+                    if(result===true)
+                        res.render('secrets')
+                })
             }
             else{
                 res.send('Invalid credentials')
